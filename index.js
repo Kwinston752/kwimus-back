882e36bffad9a7a0c300482ae70b3a20782ac122
Kwinston <yalikhan752@gmail.com>
2022-09-10 20:10:04 +0600

    Initial commit

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use(bodyParser.json())
 const routes = require('./routes/routes')
 app.use('/', routes)
 
-app.listen(process.env.PORT || 4000, () => {
-    console.log(`Сервер работает: http://localhost:${process.env.PORT || 4000}`)
+app.listen(4000, () => {
+    console.log(`Сервер работает: http://localhost:4000`)
 })
